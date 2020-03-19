f707403ff8482327c0b189417588b916e73ce0b7
Nikita Viktorov <nikita.viktorov21@gmail.com>
2020-03-20 02:34:33 +0300

    feat: add portfolio border

--- script.js
+++ script.js
@@ -6,6 +6,29 @@ MENU.addEventListener('click', (event) => {
 	event.target.classList.add('active');
 });
 
+//HEADER SCROLL
+/*
+document.addEventListener('scroll', onScroll);
+
+function onScroll(event) {
+	const cursorPos = window.scrollY;
+	const divs = document.querySelectorAll('.scroll');
+	const links = document.querySelectorAll('a');
+
+
+divs.forEach((elem) => {
+	if (elem.offsetTop <= cursorPos && (elem.offsetTop + elem.offsetHeight) > cursorPos) {
+		links.forEach((a) => {
+			a.classList.remove('active');
+			if (elem.getAttribute('id') === a.getAttribute('href').substring(1)) {
+				a.classList.add('active');
+			}
+		})
+	}
+});
+}
+*/
+
 //SCREEN OFF/ON
 const layer_h = document.getElementById('layer-h')
 const layer_v = document.getElementById('layer-v')
@@ -48,4 +71,16 @@ circle_h.addEventListener('click', (event) => {
 	else {
 		layer_h.classList.add('none');
 	}
+});
+
+//PORTFOLIO BORDER
+const portf = document.getElementById('gallery')
+
+portf.addEventListener('click', (event) => {
+	portf.querySelectorAll('img').forEach(element => {
+		element.classList.remove('border');
+		if (event.target === element) {
+			event.target.classList.add('border');
+		}
+	})
 });
